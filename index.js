1f89ebffe32b3fb3ef4dd2baa6e7f3b5f881d83c
Levy Pinheiro <111375592+Levy3006@users.noreply.github.com>
2024-11-09 18:16:27 -0300

    Update port in index.js to use vercel

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.set('view engine', 'ejs');
